879287c6244199c4ce009912297b116897660256
ssoogit <ssoogit02@gmail.com>
2023-11-14 15:52:07 +0900

    django 개발 프로젝트

---
+++ info/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class MainContent(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+# Create your models here.
---
+++ seat/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def mainpage(request) :
+    return render(request, 'mpage/mainpage.html')
+
+def service(request) :
+    return render(request, 'mpage/serv.html')
+
+def information(request) :
+    return render(request, 'mpage/info.html')
+
+def seatpage(request) :
+    return render(request, 'mpage/seat.html')
+
+def loginpage(request) :
+    return render(request, 'mpage/login.html')
+
+
+# Create your views here.
